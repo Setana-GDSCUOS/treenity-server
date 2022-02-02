6fee5c7f8f865b1a2afd13a138f9a8de872bf6ec
rxdcxdrnine <rkckr1@naver.com>
2022-02-02 22:38:59 +0900

    feat: docker-push.sh CRLF 제거 및 docker-pull.sh 추가

---
+++ docker-pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
+sudo docker rmi rkckr1/treenity-dev:latest
+sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
--- docker-push.sh
+++ docker-push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 TAG="latest"
-
+docker rmi rkckr1/treenity-dev:$TAG
 docker build -t rkckr1/treenity-dev:$TAG .
-
 docker push rkckr1/treenity-dev:$TAG
