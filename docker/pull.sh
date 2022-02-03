6b6326ed7361b845cd129ef278c38467a5fc1dd6
rxdcxdrnine <rkckr1@naver.com>
2022-02-03 23:31:28 +0900

    refactor: EOF line 추가

--- docker/pull.sh
+++ docker/pull.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ./docker
 sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
 sudo docker rmi rkckr1/treenity-dev:latest
 sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
