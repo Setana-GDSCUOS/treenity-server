252c032ada9010e1fedccc6fd6d7a112f2ef1e54
rxdcxdrnine <rkckr1@naver.com>
2022-02-03 21:15:25 +0900

    feat: springfox-boot-starter 라이브러리 추가 및swagger-ui 적용

---
+++ src/main/java/org/setana/treenity/config/WorkAround.java
@@ -0,0 +1,36 @@
+package org.setana.treenity.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+@Component
+public class WorkAround implements WebMvcOpenApiTransformationFilter {
+
+    @Value("${spring.baseUrl:${user.home}}")
+    private String serverUrl;
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openAPI = context.getSpecification();
+
+        Server server = new Server();
+        server.setDescription("base URL");
+        server.setUrl(serverUrl);
+
+        openAPI.setServers(List.of(server));
+        return openAPI;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
