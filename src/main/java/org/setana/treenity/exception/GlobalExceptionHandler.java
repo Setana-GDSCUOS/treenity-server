0d02eb1a2aa3b88e57e9aeee831a76523139cef1
Kang Changgu (Miller) <rkckr1@naver.com>
2022-03-20 23:07:47 +0900

    fix: GlobalExceptionHandler 의 response status 수정

--- src/main/java/org/setana/treenity/exception/GlobalExceptionHandler.java
+++ src/main/java/org/setana/treenity/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.setana.treenity.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +11,6 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode);
-        return new ResponseEntity<>(response, HttpStatus.valueOf(404));
+        return new ResponseEntity<>(response, errorCode.getStatus());
     }
 }
